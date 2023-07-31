4b757d3b4a409eb7170987e4e23121613ae411b5
RamaHabbal <137204591+RamaHabbal@users.noreply.github.com>
2023-08-01 02:11:06 +0300

    <frontend> modified dashboard Js, can add items to DB and display them

--- FrontEnd/script/dashboard.js
+++ FrontEnd/script/dashboard.js
@@ -1,29 +1,26 @@
-window.addEventListener('load', () => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    const authorization = JSON.parse(localStorage.getItem("authorization"));
-    const cart_id =localStorage.getItem("cart_id")
+window.addEventListener("load", async () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const authorization = JSON.parse(localStorage.getItem("authorization"));
+  // const cart_id =localStorage.getItem("cart_id")
+  // console.log(cart_id);
+  const scented = [];
+  const unscented = [];
+
+  const response = await fetch(`http://127.0.0.1:8000/api/get_category`, {
+    method: "GET",
+  });
+  const products = await response.json();
+  products.forEach((element) => {
+    const product_Id = element.id;
+    const cart_id = element.id;
     console.log(cart_id);
-    const scented = [];
-    const unscented = [];
-    async function getPproducts() {
-      try {
-        const response = await fetch(`http://127.0.0.1:8000/api/get_category`, {
-          method: "GET",
-        });
-    
-        const products = await response.json();
-    
-        products.forEach((element) => {
-          const product_Id = element.id;
-          const cart_id = element.id;
-          console.log(cart_id);
-          const name = element.name;
-          const description = element.description;
-          const image = element.image;
-          const price = element.price;
-          const category = element.category_id;
-          if (category === 1) {
-            scented.push(`
+    const name = element.name;
+    const description = element.description;
+    const image = element.image;
+    const price = element.price;
+    const category = element.category_id;
+    if (category === 1) {
+      scented.push(`
             <div class="productCard" id="${product_Id}">
             <img class="productImage" src="${image}">
             <p class="productName">${name}</p>
@@ -37,8 +34,8 @@ window.addEventListener('load', () => {
         </div>
             
         `);
-          } else if (category === 2) {
-            unscented.push(`
+    } else if (category === 2) {
+      unscented.push(`
             <div class="productCard" id="${product_Id}">
             <img class="productImage" src="${image}">
             <p class="productName">${name}</p>
@@ -52,30 +49,60 @@ window.addEventListener('load', () => {
         </div>
             
         `);
-          }
-        });
-    
-        const scentedSection = document.querySelector(".productCardContainer");
-        scentedSection.innerHTML += scented.join("");
-        const unscentedSection = document.querySelector(".productCardContainer1");
-        unscentedSection.innerHTML += unscented.join("");
-    
-        const deleteBtns = document.querySelectorAll(".deleteBtn");
-        deleteBtns.forEach((btn) =>{
-            btn.addEventListener("click",async()=>{
-            const product_Id = btn.dataset.productId;
-            const response = await fetch(`http://127.0.0.1:8000/api/deleteProduct/${product_Id}`,{
-                method: "DELETE",
-            })
-                const message = await response.json()
-                console.log(message);
-                window.location.href = "dashboard.html"
-            })
-        });
-        
-      } catch (error) {
-        console.log(error);
-      }
     }
-    getPproducts();
-    })
+  });
+
+  const scentedSection = document.querySelector(".productCardContainer");
+  scentedSection.innerHTML += scented.join("");
+  const unscentedSection = document.querySelector(".productCardContainer1");
+  unscentedSection.innerHTML += unscented.join("");
+
+  const deleteBtns = document.querySelectorAll(".deleteBtn");
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      const product_Id = btn.dataset.productId;
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/deleteProduct/${product_Id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const message = await response.json();
+      console.log(message);
+      window.location.href = "dashboard.html";
+    });
+  });
+
+  const addBtn = document.getElementById("addBtn");
+
+  addBtn.addEventListener("click", async () => {
+    const fileInput = document.getElementById("imgInput").files[0];
+    const name = document.getElementById("nameInput");
+    const price = document.getElementById("priceInput");
+    const description = document.getElementById("descriptionInput");
+    const category = document.getElementById("categoryInput");
+
+    let imgString;
+    const reader = new FileReader();
+    reader.onload = async function (e) {
+      imgString = e.target.result;
+
+      const data = {
+        name: name.value,
+        price: price.value,
+        description: description.value,
+        category_name: category.value,
+        image: imgString,
+      };
+      const response = await fetch("http://127.0.0.1:8000/api/add_product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      window.location.href = "dashboard.html";
+    };
+    reader.readAsDataURL(fileInput);
+  });
+});
